4ee820cc4cd70851df28ec88dfda12185621da61
Przemek <Przemek@HP-Przemek.(none)>
2022-10-27 19:01:24 +0200

    Project initalization

---
+++ src/main/java/com/example/milkmachineproject/MilkMachineProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.milkmachineproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MilkMachineProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MilkMachineProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/milkmachineproject/MilkMachineProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.milkmachineproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MilkMachineProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
